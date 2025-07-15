449d2a9260a723af229a17e4b4e1d4779facf71d
Silvia Arellano <silviaar@Silvias-MacBook-Pro.local>
2025-07-15 17:10:14 -0500

    repaired deploy

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/silviaarellano-de/",
+  base: process.env.NODE_ENV === 'production' ? "/silviaarellano-de/" : "/",
 });
